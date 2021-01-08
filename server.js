6d348004aa73cc2de248979f4086b0db161d279d
idsintehittapa <jblom15@gmail.com>
2021-01-08 14:13:10 +0100

    Added POST draft

--- server.js
+++ server.js
@@ -76,27 +76,64 @@ app.use((req, res, next) => {
   }
 })
 
+// // Doc
+// app.get('/', (req, res) => {
+//   res.send('Hello world')
+//   // add documentation here
+// })
+
 // GET endpoints
 // query here too RegExp too
 app.get('/testimonies', async (req, res) => {
   try {
-    const testimonies = await Testimonies.find()
+    const allTestimonies = await Testimonies.find(req.query)
       .sort({ createdAt: 'desc' })
       .limit(20)
       .exec()
-    res.status(200).json(testimonies)
+    res.status(200).json(allTestimonies)
   } catch (error) {
-    res.status(400).json({ testimonies: "could not find testimony", errors: err.errors })
+    res.status(400).json({ message: "could not find testimony", errors: err.errors })
   }
 })
 
-// Doc
-app.get('/', (req, res) => {
-  res.send('Hello world')
-  // add documentation here
-})
+// GET returns one object from the database via ID
+// app.get('/testimonies/:id', async (req, res) => {
+//   try {
+//     const singleTestimony = await Testimonies.findOne({ id: req.params.id })
+//     if (singleTestimony) {
+//       res.json(singleTestimony)
+//       res.status(200).json(singleTestimony)
+//     } else {
+//       res.status(404).json({ error: 'testimony not found' })
+//     }
+//   } catch (err) {
+//     res.status(400).json({ errors: err.errors })
+//   }
+// })
 
 // POST endpoints
+app.post('/testimonies', async (req, res) => {
+  // send a request body in order to pass information into the API
+  try {
+    // success case
+    const NewTestimony = new Message({ 
+      name: req.body.name, 
+      when_weeks: req.body.when_weeks, 
+      when_weeks_noticed: req.body.when_weeks_noticed,
+      physical_pain: req.body.physical_pain,
+      mental_pain: req.body.mental_pain,
+      hospital: req.body.hospital,
+      period_volume: req.body.period_volume,
+      period_length: req.body.period_length,
+      period_pain: req.body.period_pain,
+      story: req.body.story,})
+    const savedTestimony = await NewTestimony.save()
+    res.status(200).json(savedTestimony)
+  } catch (err) {
+    // Bad request - notify the client that attempt to post was unsuccessful
+    res.status(400).json({ message: "could not save testimony", errors: err.errors })
+  }
+})
 
 // Start the server
 app.listen(port, () => {
